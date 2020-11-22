e301959037523b1ebcf0cce826ba70b6589d6bd5
LinZong <353388639@qq.com>
2020-11-22 23:43:27 +0800

    Help you copy command to clipboard for convenience

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -1,5 +1,7 @@
 package demo;
 
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
 import java.io.IOException;
 import java.lang.instrument.UnmodifiableClassException;
 
@@ -8,6 +10,9 @@ public class App {
 	public static void main(String[] args) throws IOException {
 		System.out.println("Please use Arthas 3.3.9 and enter command `trace demo.OomClass traceMe`");
 		System.out.println("请使用Arthas 3.3.9 版本，输入命令 `trace demo.OomClass traceMe`");
+		System.out.println("This command is copied to your clipboard for your convenience. ^_^");
+		System.out.println("已经帮你把命令复制到了剪贴板啦! ^_^");
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection("trace demo.OomClass traceMe"),null);
 		// Trigger class to load.
 		Class<OomClass> oomClazz = OomClass.class;
 		System.in.read();
