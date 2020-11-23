0f35b89428b321a2a46207957df293216f402b56
LinZong <353388639@qq.com>
2020-11-23 09:22:30 +0800

    Be compatible with no X11 display variable

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -10,12 +10,18 @@ public class App {
 	public static void main(String[] args) throws IOException {
 		System.out.println("Please use Arthas 3.3.9 and enter command `trace demo.OomClass traceMe`");
 		System.out.println("请使用Arthas 3.3.9 版本，输入命令 `trace demo.OomClass traceMe`");
-		System.out.println("This command is copied to your clipboard for your convenience. ^_^");
-		System.out.println("已经帮你把命令复制到了剪贴板啦! ^_^");
-		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection("trace demo.OomClass traceMe"),null);
 		// Trigger class to load.
+		tryCopyCommandToClipboard();
 		Class<OomClass> oomClazz = OomClass.class;
 		System.in.read();
 		System.out.println("Goodbye! >_<");
+	}
+
+	private static void tryCopyCommandToClipboard() {
+		try {
+			Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection("trace demo.OomClass traceMe"),null);
+			System.out.println("This command is copied to your clipboard for your convenience. ^_^");
+			System.out.println("已经帮你把命令复制到了剪贴板啦! ^_^");
+		} catch (Exception ignored) {}
 	}
 }
